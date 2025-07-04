0bcc3f74378aea6750a4110a11871bc4570d7da4
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2025-07-03 20:46:07 -0600

    Primeros cambios header y Formulario

--- src/app/kodeo-website/default/sections/contactus-banner-kodeo-website/contactus-banner-kodeo-website.component.ts
+++ src/app/kodeo-website/default/sections/contactus-banner-kodeo-website/contactus-banner-kodeo-website.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ScrollServiceService } from '../../../../services/scroll-service.service';
+import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus-banner-kodeo-website',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contactus-banner-kodeo-website.component.html',
   styleUrl: './contactus-banner-kodeo-website.component.scss'
 })
 export class ContactusBannerKodeoWebsiteComponent {
+  private subscription!: Subscription;
+  form: FormGroup;
+  
+  constructor(private scrollService: ScrollServiceService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: [''],
+      message: ['', Validators.required]
+    });
+  }
 
+  ngOnInit() {
+    this.subscription = this.scrollService.fillContactForm$.subscribe(text => {
+      console.log( text);
+      this.form.get('subject')?.setValue(text);
+    });
+  }
 }
